d516917ab71e1243027529b6b9af61c5e07b36b4
cho eun jae <jej059813@naver.com>
2025-10-25 17:48:00 +0900

    ScientificCalculator 주석 추가 완료

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -1,4 +1,8 @@
+// 계산기 v3.0 관련 클래스
+// https://github.com/choeunjae/Java-Calculator-04/issues/8
 public class ScientificCalculator extends Calculator {
+    // 계산기 v3.0 팩토리 기능 개발 완료
+    // https://github.com/choeunjae/Java-Calculator-04/issues/9
     @Override
     public int factorial(){
 
