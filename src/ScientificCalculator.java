b7fcd8573ac0d44f8d11cfd70a7652a73cf30697
cho eun jae <jej059813@naver.com>
2025-10-25 17:57:48 +0900

    num1 의 값만 받아오는
        public void setNumbers(int num1) {
            this.num1 = num1;
        }
    코드 추가

--- src/ScientificCalculator.java
+++ src/ScientificCalculator.java
@@ -3,6 +3,11 @@
 public class ScientificCalculator extends Calculator {
     // 계산기 v3.0 팩토리 기능 개발 완료
     // https://github.com/choeunjae/Java-Calculator-04/issues/9
+
+    public void setNumbers(int num1) {
+        this.num1 = num1;
+    }
+
     @Override
     public int factorial(){
 
