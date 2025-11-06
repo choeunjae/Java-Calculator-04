9d03de3a8ab56aadde3d48daf58ecaed7d66716f
johaeun301 <haeunj125@gmail.com>
2025-11-06 10:17:58 +0900

    Circle 단위 테스트 완료

---
+++ test/CircleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+class CircleTest {
+    @Test
+    void carcCircletest() {
+        Circle circle = new Circle();
+        circle.setNumbers(10);
+        circle.calcCircle();
+    }
+}
